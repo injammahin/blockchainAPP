147fac9bc09c20c40c3419f31ecf04f0da23cc68
injammahin <injammahin5507@gmail.com>
2024-02-05 17:48:59 +0600

    first commit

---
+++ client/src/App.jsx
@@ -0,0 +1,26 @@
+import {
+  Navbar,
+  Loader,
+  Welcome,
+  Service,
+  Transactions,
+  Footer,
+} from "./components";
+
+const App = () => {
+  return (
+    <>
+      <div className="min-h-screen">
+        <div className="gradient-bg-welcome">
+          <Navbar />
+          <Welcome />
+        </div>
+        <Service />
+        <Transactions />
+        <Footer />
+      </div>
+    </>
+  );
+};
+
+export default App;
